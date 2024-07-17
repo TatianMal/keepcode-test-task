ab79d0e3f4a2fa502818f62b67082dab1c4238f2
Tatiana Maltseva <tatianmaltseva@gmail.com>
2024-07-17 16:47:24 +0300

    (components student): add vue-select and add local filter logic

--- src/components/student/StudentDocumentList.vue
+++ src/components/student/StudentDocumentList.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { defineEmits, onBeforeMount, ref } from 'vue'
+import { defineEmits, onBeforeMount, ref, type Ref, computed } from 'vue'
 
 import BaseModal from '@/components/base/BaseModal.vue'
 
@@ -8,15 +8,15 @@ import StudentDocumentCreate from '@/components/student/StudentDocumentCreate.vu
 
 import type { StudentDocument as StudentDocumentModel } from '@/api/models/StudentDocument'
 import { useStudentDocuments } from '@/components/student/composables'
+import { StudentDocumentStatus } from '@/api/models/StudentDocumentStatus'
+import { StudentDocumentType } from '@/api/models/StudentDocumentType'
 
 const { getDocuments, createDocument } = useStudentDocuments()
 const documents = ref<Array<StudentDocumentModel>>([])
-// отдельный ref для ускорения разработки - computed?
-const filteredDocuments = ref<Array<StudentDocumentModel>>([])
+
 onBeforeMount(async () => {
   try {
     documents.value = await getDocuments()
-    filteredDocuments.value = [...documents.value]
   } catch (error) {
     console.error(error)
   }
@@ -26,11 +26,60 @@ const createAndAddDocument = async (body: any) => {
   try {
     const newDoc = await createDocument(body)
     documents.value.push(newDoc)
-    filteredDocuments.value = [...documents.value]
   } catch (error) {
     console.error(error)
   }
 }
+
+const docTypes = [
+  {
+    label: 'Договор',
+    value: StudentDocumentType.CONTRACT
+  },
+  {
+    label: 'Справка',
+    value: StudentDocumentType.ENQUIRY
+  },
+  {
+    label: 'Другое',
+    value: StudentDocumentType.OTHER
+  }
+]
+
+const docStatuses = [
+  {
+    label: 'Заключен',
+    value: StudentDocumentStatus.CONCLUDED
+  },
+  {
+    label: 'Расторгнут',
+    value: StudentDocumentStatus.TERMINATED
+  }
+]
+
+const sortOptions = [{ value: 'date', label: 'Дате' }]
+
+const filters: Ref<Record<string, string | undefined>> = ref({
+  type: undefined,
+  status: undefined,
+  sort: sortOptions[0].value
+})
+
+const reducer = (type: (typeof sortOptions)[0]) => type.value
+
+const filteredDocuments = computed<Array<StudentDocumentModel>>(() => {
+  if (documents.value.length === 0) {
+    return []
+  }
+  let res = [...documents.value]
+  if (filters.value.type) {
+    res = res.filter((doc) => doc.type === filters.value.type)
+  }
+  if (filters.value.status) {
+    res = res.filter((doc) => doc.status === filters.value.status)
+  }
+  return res
+})
 
 const modal = ref<InstanceType<typeof BaseModal>>()
 const showModal = () => {
@@ -42,9 +91,29 @@ const showModal = () => {
   <article class="document-info">
     <section class="document-info__actions">
       <section class="filter-list">
-        <div class="filter">Тип документа Не выбрано Договор Справка Анкета</div>
-        <div class="filter">Статус Не выбрано Заключен Расторгнут</div>
-        <div class="filter">Сортировать по Дате Типу документа</div>
+        <div class="filter">
+          Тип документа
+          <v-select
+            v-model="filters.type"
+            label="label"
+            :reduce="reducer"
+            placeholder="Не выбрано"
+            :options="docTypes"
+          ></v-select>
+        </div>
+        <div class="filter">
+          Статус
+          <v-select
+            v-model="filters.status"
+            label="label"
+            placeholder="Не выбрано"
+            :options="docStatuses"
+          ></v-select>
+        </div>
+        <div class="filter">
+          Сортировать по
+          <v-select :value="filters.sort" label="label" :options="sortOptions" disabled></v-select>
+        </div>
       </section>
       <button class="primary-action-btn" @click="showModal">добавить документ</button>
     </section>
@@ -81,6 +150,12 @@ const showModal = () => {
   display: flex;
   flex-direction: row;
   gap: 164px;
+}
+
+.filter {
+  display: flex;
+  flex-direction: column;
+  width: 100px;
 }
 
 .doc-list {
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import './assets/main.css'
+import 'vue-select/dist/vue-select.css'
 
+import VueSelect from 'vue-select'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).component('v-select', VueSelect).mount('#app')
